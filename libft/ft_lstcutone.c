9d2dee35c117018899ce7f8dec18ae7647ecb851
ezalos <ezalos@gmail.com>
2018-12-07 13:31:07 +0100

    lst cut one

--- libft/ft_lstcutone.c
+++ libft/ft_lstcutone.c
@@ -6,13 +6,13 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 01:53:37 by ldevelle          #+#    #+#             */
-/*   Updated: 2018/12/07 01:53:39 by ldevelle         ###   ########.fr       */
+/*   Updated: 2018/12/07 13:30:54 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+void	ft_lstcutone(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list	*ptr;
 
