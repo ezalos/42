6a36bf11d4ac65c786e03f68a9787687feb651f9
ezalos <ezalos@gmail.com>
2018-12-09 15:50:31 +0100

    ready to be tested under UNIX/LINUX

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 17:03:44 by ldevelle          #+#    #+#             */
-/*   Updated: 2018/12/09 15:43:05 by ldevelle         ###   ########.fr       */
+/*   Updated: 2018/12/09 15:49:12 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@
 # include <errno.h>
 # include <stdio.h>
 
-//gcc main.c get_next_line.c get_next_line.h libft.a libft.h ..\libft\ft_strlen.c ..\libft\ft_strsub.c ..\libft\ft_strchr.c ..\libft\ft_memmove.c ..\libft\ft_memalloc.c ..\libft\ft_strcpy.c ..\libft\ft_strlcat.c ..\libft\ft_lstadd.c ..\libft\ft_lstnew.c ..\libft\ft_strnew.c ..\libft\ft_strncpy.c ..\libft\ft_memcpy.c
+//gcc main.c get_next_line.c get_next_line.h libft.a libft.h
+
+//gcc main.c get_next_line.c get_next_line.h libft.h ..\libft\ft_strlen.c ..\libft\ft_strsub.c ..\libft\ft_strchr.c ..\libft\ft_memmove.c ..\libft\ft_memalloc.c ..\libft\ft_strcpy.c ..\libft\ft_strlcat.c ..\libft\ft_lstadd.c ..\libft\ft_lstnew.c ..\libft\ft_strnew.c ..\libft\ft_strncpy.c ..\libft\ft_memcpy.c
 
 int		get_next_line(const int fd, char **line);
 
