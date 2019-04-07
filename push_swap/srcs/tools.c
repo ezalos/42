e7d941bc22a151ee79bf43c0209d6079bcda21cf
Louis DEVELLE <ldevelle@e2r2p8.42.fr>
2019-04-07 19:49:46 +0200

    PushSwap Good but too speaky Libft new function in test

--- push_swap/srcs/tools.c
+++ push_swap/srcs/tools.c
@@ -6,7 +6,7 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/31 00:04:27 by ldevelle          #+#    #+#             */
-/*   Updated: 2019/04/03 20:33:03 by ldevelle         ###   ########.fr       */
+/*   Updated: 2019/04/07 13:27:59 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int		order_a(int size)
 
 	i = -1;
 	push = *ft_remember_push();
+	if (size > (int)push->size_a)
+		size = (int)push->size_a;
 	while (++i < size)
 	{
 		actual = *(int*)ft_tab_dirth(push->stack_a, 0, i)->content;
@@ -40,6 +42,8 @@ int		order_b(int size)
 
 	i = -1;
 	push = *ft_remember_push();
+	if (size > (int)push->size_b)
+		size = (int)push->size_b;
 	while (++i < size)
 	{
 		actual = *(int*)ft_tab_dirth(push->stack_b, 0, i)->content;
@@ -55,13 +59,30 @@ int		ft_get_median(t_tab *stack, size_t size)
 	int *squick;
 	int i;
 
-	squick = ft_memalloc(sizeof(int) * (size + 1));
+	squick = ft_memalloc(sizeof(int) * (size));
 	i = -1;
 	while (++i < (int)size)
 		squick[i] = *(int*)ft_tab_dirth(stack, 0, i)->content;
-	ft_squick_sort(&squick, 0, size);
-	i = squick[size / 2];
+	i = -1;
+	// CLEAR_SCREEN
+	// print_push_swap((*ft_remember_push()));
+	// ft_place_cursor(3, 0);
+	// while (++i < (int)size)
+	// 		ft_printf("%~{}[%d]\t= %-5d\n", i, squick[i]);
+	ft_squick_sort(&squick, 0, size - 1);
+//	ft_printf("MEDIAN\tsize %d -> [%d]=%d\n", size, (size - 1) / 2, squick[(size - 1) / 2]);
+	// i = -1;
+	// while (++i < (int)size)
+	// 	if (i == (size - 1) / 2)
+	// 		ft_printf("%~{150;255;150}[%d]\t= %-5d\n", i, squick[i]);
+	// 	else
+	// 		ft_printf("%~{255;150;150}[%d]\t= %-5d\n", i, squick[i]);
+	if (size)
+		i = squick[(size - 1) / 2];
+	else
+		return (-1);
 	ft_memdel((void**)&squick);
+	// ft_press_any_key();
 	return (i);
 }
 
@@ -73,61 +94,6 @@ int		median_a(size_t size)
 int		median_b(size_t size)
 {
 	return (ft_get_median((*ft_remember_push())->stack_b, size));
-}
-
-int		ft_mv(t_push_swap *push, size_t size, int a)
-{
-	int 	i;
-	int 	done;
-	int		median;
-
-	if (!size)
-		return (0);
-	if ((a && (size > push->size_a)) || (!a && (size > push->size_b)))
-		return (0);
-	if (size == 1)
-	{
-		if (a)
-			pa();
-		else
-			pb();
-		return (1);
-	}
-	i = 0;
-	done = 0;
-	if (a)
-		median = median_a(size);
-	else
-		median = median_b(size);
-	while (i < (int)size && (i - done) < (int)size / 2)
-	{
-		if (a)
-		{
-			if (stack_a(0) <= median)
-				pa();
-			else
-			{
-				ra();
-				done++;
-			}
-		}
-		else
-			if (stack_b(0) > median)
-				pb();
-			else
-			{
-				rb();
-				done++;
-			}
-		i++;
-	}
-	while (done--)
-		if (a)
-			rra();
-		else
-			rrb();
-	// print_push_swap(push);
-	return (size / 2);
 }
 
 int			stack_a(int i)
@@ -162,9 +128,9 @@ int		ab(size_t size)
 		if (stack_a(0) >= stack_a(1))
 			sa();
 		pa();
-		if (stack_a(0) >= stack_a(1))
-			return (2);
-		else
+		// if (stack_a(0) >= stack_a(1))
+		// 	return (2);
+		// else
 			return (1);
 	}
 	r_v = ft_mv(*ft_remember_push(), size, 1);
@@ -187,9 +153,9 @@ int		ba(size_t size)
 		if (stack_b(0) < stack_b(1))
 			sb();
 		pb();
-		if (stack_b(0) < stack_b(1))
-			return (2);
-		else
+		// if (stack_b(0) < stack_b(1))
+		// 	return (2);
+		// else
 			return (1);
 	}
 	r_v = ft_mv(*ft_remember_push(), size, 0);
